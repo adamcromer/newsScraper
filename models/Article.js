d47abfa2cbdf463798ce4ec200838a81199e805a
adamjorycromer <adamjorycromer@gmail.com>
2019-03-18 18:35:26 -0600

    files started

---
+++ models/Article.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+var ArticleSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
